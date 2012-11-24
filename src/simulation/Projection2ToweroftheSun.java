143cb3fb579264ab73ba38ed093ea788634230be
Atsushi Tadokoro <tadokoro@gmail.com>
2012-11-25 03:36:21 +0900

    fix indent

--- src/simulation/Projection2ToweroftheSun.java
+++ src/simulation/Projection2ToweroftheSun.java
@@ -1,5 +1,5 @@
 /*
- * 東京ビッグサイトにプロジェクションマッピング
+ * 太陽の塔にプロジェクションマッピング
  *
  */
 
@@ -33,31 +33,31 @@ public class Projection2ToweroftheSun extends SimpleApplication {
 	Spatial[] girl = new Spatial[NUM];
 	PApplet applet;
 	PAppletProjectorShadowNode ppn;
-
+	
 	@Override
-    // 初期設定
+	// 初期設定
 	public void simpleInitApp() {
 		setupEnvironment();
 		setupProjector();
 	}
-
-    // 環境の設定
+	
+	// 環境の設定
 	private void setupEnvironment(){
 		// カメラ設定
 		cam.setLocation(new Vector3f(0, 10, 150));
 		cam.lookAt(new Vector3f(10, 60, 0), Vector3f.UNIT_Y);
 		flyCam.setMoveSpeed(20);
 		flyCam.setDragToRotate(true);
-        
+		
 		// ライティング
 		DirectionalLight sun = new DirectionalLight();
-		sun.setColor(ColorRGBA.White.mult(0.3f));
+		sun.setColor(ColorRGBA.White.mult(1.5f));
 		sun.setDirection(new Vector3f(0f, 1.0f, -0.75f));
 		rootNode.addLight(sun);
 		AmbientLight al = new AmbientLight();
 		al.setColor(ColorRGBA.White.mult(0.1f));
 		rootNode.addLight(al);
-
+		
 		// 空を設定
 		Texture west = assetManager.loadTexture("Textures/Sky/Lagoon/lagoon_west.jpg");
 		Texture east = assetManager.loadTexture("Textures/Sky/Lagoon/lagoon_east.jpg");
@@ -67,17 +67,17 @@ public class Projection2ToweroftheSun extends SimpleApplication {
 		Texture down = assetManager.loadTexture("Textures/Sky/Lagoon/lagoon_down.jpg");
 		Spatial sky = SkyFactory.createSky(assetManager, west, east, north, south, up, down);
 		rootNode.attachChild(sky);
-
-        // マテリアル
+		
+		// マテリアル
 		Material textureMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
 		textureMat.setTexture("ColorMap", assetManager.loadTexture("myAssets/Textures/metalTexture.jpg"));
 		
-        // 地面を設定
+		// 地面を設定
 		Box floor = new Box(Vector3f.ZERO, 1500f, 0.01f, 1500f);
 		Geometry floorGeom = new Geometry("Floor", floor);
 		floorGeom.setMaterial(textureMat);
 		rootNode.attachChild(floorGeom);
-
+		
 		// ランダムに100人の女性を配置
 		for(int i = 0; i < girl.length; i++) {
 			girl[i] = assetManager.loadModel("myAssets/Models/WalkingGirl/WalkingGirl.obj");
@@ -86,41 +86,42 @@ public class Projection2ToweroftheSun extends SimpleApplication {
 			girl[i].setLocalTranslation(girlPos);
 			this.rootNode.attachChild(girl[i]);
 		}
-        
-		// ビッグサイトを配置
+		
+		// 太陽の塔を配置
 		Spatial object = (Spatial) assetManager.loadModel("myAssets/Models/TowerofTheSun/TowerofTheSun.obj");
 		rootNode.attachChild(object);
-        
-        // プロジェクタの映像を影を表示
+		
+		// プロジェクタの映像を影を表示
 		object.setShadowMode(ShadowMode.CastAndReceive);
 		floorGeom.setShadowMode(ShadowMode.CastAndReceive);
 	}
-
-    // プロジェクタ設定
+	
+	// プロジェクタ設定
 	private void setupProjector(){
-        //projector(new!)
-        //PApplet applet = new P_2_1_1_03();
+		// Appletを選択
+		PApplet applet = new P_2_1_1_03();
+		//PApplet applet = new P_2_2_5_01();
 		//PApplet applet = new ColorBarsPApplet();
-		PApplet applet = new P_2_2_5_01();
-        PAppletProjectorNode projectorNode = new PAppletProjectorNode("projector0", assetManager, applet, 400, 400, true);
-        projectorNode.setLocalTranslation(new Vector3f(0, 4f, 200));
-        projectorNode.lookAt(new Vector3f(0, 50f, 0f), Vector3f.UNIT_Y);
-        rootNode.attachChild(projectorNode);
-        rootNode.attachChild(projectorNode.getFrustmMdel()); // if you don't want to see frustum, please don't attach it to rootNode. 
-        //projector should be added to TextureProjectorRenderer, and TextureProjectorRenderer should be added to ViewPort.
+		
+		// 新プロジェクターを配置
+		PAppletProjectorNode projectorNode = new PAppletProjectorNode("projector0", assetManager, applet, 400, 400, true);
+		projectorNode.setLocalTranslation(new Vector3f(0, 4f, 150));
+		projectorNode.lookAt(new Vector3f(0, 40f, 0f), Vector3f.UNIT_Y);
+		rootNode.attachChild(projectorNode);
+		rootNode.attachChild(projectorNode.getFrustmMdel());
 		TextureProjectorRenderer ptr = new TextureProjectorRenderer(assetManager);
 		ptr.getTextureProjectors().add(projectorNode.getProjector());
 		viewPort.addProcessor(ptr);
 	}
-
-    @Override
-    // 終了処理
+	
+	@Override
+	// 終了処理
 	public void destroy() {
 		super.destroy();
 		System.exit(0);
 	}
 	
-    // メイン
+	// メイン
 	public static void main(String[] args){
 		SimpleApplication app = new Projection2ToweroftheSun();
 		app.setPauseOnLostFocus(false); 
